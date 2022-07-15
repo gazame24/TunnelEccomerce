f7af31d155372bb7c00ec639484de0ce12572760
gazame24 <kenneth.gaza93@gmail.com>
2022-07-15 17:08:21 +0800

    Commit New Project innit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <Navbar />
+  )
+}
+
+export default App
